83f302506550599cb3d0e7b687319eebe1d381f5
roggervasquez <rogger.vasquez@gmail.com>
2016-06-26 11:16:47 -0600

    initial commit, adding files

---
+++ go.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#Delete *.class
+rm *.class
+
+#Compile the Challenge.java that has all the logic, but uses the minimal-json-0.9.4 to parse Json objects
+javac -cp ./minimal-json-0.9.4.jar:. Challenge.java
+
+#Run the application, pass the 2 files as parameters,
+java -cp  ./minimal-json-0.9.4.jar:. Challenge products.txt listings.txt
